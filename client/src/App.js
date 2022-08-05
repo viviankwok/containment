7c8540d557247c2d237d9a243b6e90a2d36de12d
viviankwok <104907963+viviankwok@users.noreply.github.com>
2022-08-06 06:18:08 +0800

    imported material ui mods

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Main from "./components/Main";
 import NavBar from "./components/NavBar";
 
+import { BottomNavigation, BottomNavigationAction } from "@mui/material";
+import { Icon } from "@mui/material";
+
 const App = () => {
+  const [value, setValue] = useState("testing");
+
   return (
     <div id="app">
       App component here
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        {/* <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} /> */}
+
+        <BottomNavigationAction label="Recents" />
+        <BottomNavigationAction label="Favorites" />
+        <BottomNavigationAction label="Nearby" />
+      </BottomNavigation>
       <NavBar />
       <Main />
     </div>
