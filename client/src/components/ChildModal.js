a36d4b9725780e151ba90d647608391845d6244c
viviankwok <104907963+viviankwok@users.noreply.github.com>
2022-08-09 01:35:16 +0800

    [fe] updated containers tab
    show all;
    parent modal - product info;
    child modal - delete confirmation

---
+++ client/src/components/ChildModal.js
@@ -0,0 +1,82 @@
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Button from "@mui/material/Button";
+import { Typography } from "@mui/material";
+import { useContext } from "react";
+import ReactContext from "./context/react.context";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+};
+
+export default function ChildModal(props) {
+  const reactCtx = useContext(ReactContext);
+
+  const handleClose = () => {
+    props.setOpenChild(false);
+  };
+
+  const confirmDelete = (productCode) => {
+    props.delData(productCode);
+    props.setOpenChild(false);
+    reactCtx.setOpen(false);
+  };
+
+  return (
+    <div>
+      {/* <Button onClick={handleOpen}>Open Child Modal</Button> */}
+      <Modal
+        hideBackdrop
+        open={props.openChild}
+        onClose={handleClose}
+        aria-labelledby="child-modal-title"
+        aria-describedby="child-modal-description"
+      >
+        <Box sx={{ ...style, width: 500 }}>
+          <Typography
+            id="child-modal-title"
+            variant="overline"
+            component="h2"
+            align="center"
+            sx={{ mb: 2 }}
+          >
+            [Warning]
+          </Typography>
+          <div id="child-modal-description" className="flex justify center">
+            This will delete
+            <span className="font-bold uppercase">
+              &nbsp;{reactCtx.modalProduct.name}&nbsp;
+            </span>
+            permanently. Are you sure?
+          </div>
+          <br />
+          <div className="flex justify-center">
+            <Button variant="contained" onClick={handleClose}>
+              Back
+            </Button>
+            &nbsp;&nbsp;&nbsp;
+            <Button
+              variant="contained"
+              onClick={() => {
+                confirmDelete(reactCtx.modalProduct.product_code);
+              }}
+              color="warning"
+            >
+              Delete permanently
+            </Button>
+          </div>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
--- client/src/components/Containers.js
+++ client/src/components/Containers.js
@@ -1,7 +1,106 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ContainerModal from "./ContainerModal";
+import {
+  ImageList,
+  ImageListItem,
+  Typography,
+  ImageListItemBar,
+} from "@mui/material";
+import ReactContext from "./context/react.context";
 
 const Containers = () => {
-  return <div id="containers">Containers component</div>;
+  const [containers, setContainers] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [modalProduct, setModalProduct] = useState("");
+
+  // container info modal
+  const handleOpen = (d) => {
+    setOpen(true);
+    setModalProduct(d);
+  };
+  const handleClose = () => setOpen(false);
+
+  // fetch all containers from db
+  const getData = async () => {
+    // endpoint URL
+    const url = "http://localhost:5001/containers/all";
+    // fetch config
+    const config = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        // authorization: "Bearer" + accessToken,
+      },
+      // body: JSON.stringify("content"),
+    };
+
+    // fetch
+    try {
+      const res = await fetch(url, config);
+      if (res.status !== 200) {
+        throw new Error(
+          "Something went wrong - msg from FE fetching at Containers.js"
+        );
+      }
+
+      const data = await res.json();
+      console.log("data fetched from BE: ", data);
+      setContainers(data);
+      // setContainers(JSON.stringify(data));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div id="containers" className=" justify-center">
+      <ReactContext.Provider
+        value={{ open, setOpen, modalProduct, setModalProduct }}
+      >
+        <div className="mb-1 grid justify-center">
+          <div className="text-2xl font-bold">All containers.</div>
+          <div className="text-xs font-light">[EVERYTHING OUT THERE]</div>
+        </div>
+        <ContainerModal
+          open={open}
+          handleOpen={handleOpen}
+          handleClose={handleClose}
+          modalProduct={modalProduct}
+        />
+        <br />
+
+        <ImageList sx={{ width: 1, height: 700 }} cols={5} gap={25}>
+          {containers.map((d, i) => (
+            <ImageListItem key={d.product_code}>
+              <img
+                src={`${d.img}?w=164&h=164&fit=crop&auto=format`}
+                srcSet={`${d.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                alt={d.name}
+                loading="lazy"
+                className="rounded-md drop-shadow-md"
+                onClick={() => {
+                  handleOpen(d);
+                }}
+              />
+              <Typography
+                variant="caption"
+                align="center"
+                sx={{ fontWeight: "bold", mt: 1 }}
+              >
+                {d.name}
+                <br />
+                <span className="font-normal">by {d.brand}</span>
+              </Typography>
+            </ImageListItem>
+          ))}
+        </ImageList>
+      </ReactContext.Provider>
+    </div>
+  );
 };
 
 export default Containers;
